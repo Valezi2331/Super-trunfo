ee21b9da68700420316bb314227270638b8581a6
Leonardo Valezi <148929677+Valezi2331@users.noreply.github.com>
2025-08-14 21:46:26 -0300

    Add files via upload

---
+++ SuperTrunfo.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+int main(){
+
+char estado1;
+char cod1;
+char cidade1;
+int populacao1;
+float area1;
+float pib1;
+int turistico1;
+
+char estado2;
+char cod2;
+char cidade2;
+int populacao2;
+float area2;
+float pib2;
+int turistico2;
+
+printf("Vamos introudzir os dados da carta 1\n");
+
+printf("Insira o estado(A a H) \n");
+scanf("%c", &estado1);
+
+printf("Insira o código da carta \n");
+scanf("%s", &cod1);
+
+printf("Insira a cidade \n");
+scanf("%s", &cidade1);
+
+printf("Insira a população \n");
+scanf("%d", &populacao1);
+
+printf("Insira a área da cidade \n");
+scanf("%f", &area1);
+
+printf("Insira o PIB \n");
+scanf("%f", &pib1);
+
+printf("Insira a quantidade de pontos jurídicos \n");
+scanf("%d", &turistico1);
+
+printf("Agora vamos fazer a carta 2 \n");
+
+printf("Insira o estado(A a H) \n");
+scanf("%s", &estado2);
+
+printf("Insira o código da carta \n");
+scanf("%s", &cod2);
+
+printf("Insira a cidade \n");
+scanf("%s", &cidade2);
+
+printf("Insira a população \n");
+scanf("%d", &populacao2);
+
+printf("Insira a área da cidade \n");
+scanf("%f", &area2);
+
+printf("Insira o PIB \n");
+scanf("%f", &pib2);
+
+printf("Insira a quantidade de pontos jurídicos \n");
+scanf("%d", &turistico2);
+
+    printf("Informações da carta 1 \n");
+    printf("Estado: %c \n", estado1);
+    printf("Código: %s \n", cod1);
+    printf("Cidade: %s \n", cidade1);
+    printf("População: %d \n", populacao1);
+    printf("Área: %f \n", area1);
+    printf("PIB: %f \n", pib1);
+    printf("Quantidade de pontos turísticos: %d \n", turistico1);
+    printf("\n"); 
+
+    printf("Informações da carta 2 \n");
+    printf("Estado: %c \n", estado2);
+    printf("Código: %s \n", cod2);
+    printf("Cidade: %s \n", cidade2);
+    printf("População: %d \n", populacao2);
+    printf("Área: %f \n", area2);
+    printf("PIB: %f \n", pib2);
+    printf("Quantidade de pontos turísticos: %d \n", turistico2);
+
+}
