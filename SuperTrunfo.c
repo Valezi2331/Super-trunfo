1cfa62fe8ab0e8fb95a6b2a57bffd0be32b138d8
Leonardo Valezi <148929677+Valezi2331@users.noreply.github.com>
2025-08-15 10:31:19 -0300

    Update SuperTrunfo.c
    Adicionado funções calculo pib per capita e densidade populacional

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -64,23 +64,45 @@ scanf("%f", &pib2);
 printf("Insira a quantidade de pontos jurídicos \n");
 scanf("%d", &turistico2);
 
-    printf("Informações da carta 1 \n");
-    printf("Estado: %c \n", estado1);
-    printf("Código: %s \n", cod1);
-    printf("Cidade: %s \n", cidade1);
-    printf("População: %d \n", populacao1);
-    printf("Área: %f \n", area1);
-    printf("PIB: %f \n", pib1);
-    printf("Quantidade de pontos turísticos: %d \n", turistico1);
-    printf("\n"); 
 
-    printf("Informações da carta 2 \n");
-    printf("Estado: %c \n", estado2);
-    printf("Código: %s \n", cod2);
-    printf("Cidade: %s \n", cidade2);
-    printf("População: %d \n", populacao2);
-    printf("Área: %f \n", area2);
-    printf("PIB: %f \n", pib2);
-    printf("Quantidade de pontos turísticos: %d \n", turistico2);
+
+float densidade1;
+float densidade2;
+
+densidade1= (populacao1 / area1);
+densidade2= (populacao2 / area2);
+
+
+
+float pibcapita1;
+float pibcapita2;
+
+pibcapita1= (pib1 / populacao1);
+pibcapita2= (pib2 / populacao2);    
+
+
+
+printf("Informações da carta 1 \n");
+printf("Estado: %c \n", estado1);
+printf("Código: %s \n", cod1);
+printf("Cidade: %s \n", cidade1);
+printf("População: %d \n", populacao1);
+printf("Área: %f \n", area1);
+printf("PIB: %f \n", pib1);
+printf("Quantidade de pontos turísticos: %d \n", turistico1);
+printf("Densidade populacional: %f \n", densidade1);
+printf("Pib per Capita: %f \n ", pibcapita1);
+printf("\n"); 
+
+printf("Informações da carta 2 \n");
+printf("Estado: %c \n", estado2);
+printf("Código: %s \n", cod2);
+printf("Cidade: %s \n", cidade2);
+printf("População: %d \n", populacao2);
+printf("Área: %f \n", area2);
+printf("PIB: %f \n", pib2);
+printf("Quantidade de pontos turísticos: %d \n", turistico2);
+printf("Densidade populacional: %f \n", densidade2);
+printf("Pib per Capita: %f \n ", pibcapita2);
 
 }
